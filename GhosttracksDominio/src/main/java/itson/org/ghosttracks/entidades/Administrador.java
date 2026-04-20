9eb39a67dc43426e402c32607eed48cc5efc75b1
michelle8q <cincastle584@gmail.com>
2026-04-19 20:11:08 -0700

    Entidades

---
+++ GhosttracksDominio/src/main/java/itson/org/ghosttracks/entidades/Administrador.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package itson.org.ghosttracks.entidades;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ *
+ * @author cinca
+ */
+public class Administrador {
+    
+    @Id
+    private Long idEmpleado;
+    
+    private String nombre;
+    private String correo;
+    private String telefono;
+    
+    public Administrador() {
+        
+    }
+
+    public Administrador(String nombre, String correo, String telefono) {
+        this.nombre = nombre;
+        this.correo = correo;
+        this.telefono = telefono;
+    }
+
+    public Administrador(Long idEmpleado, String nombre, String correo, String telefono) {
+        this.idEmpleado = idEmpleado;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.telefono = telefono;
+    }
+
+    public Long getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public void setIdEmpleado(Long idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    
+    
+}
--- GhosttracksDominio/src/main/java/itson/org/ghosttracks/entidades/Producto.java
+++ GhosttracksDominio/src/main/java/itson/org/ghosttracks/entidades/Producto.java
@@ -5,13 +5,19 @@ import itson.org.ghosttracks.enums.TipoProducto;
 
 import java.util.List;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 /**
  * Entidad de Dominio
  * @author Emy
  */
+@Document(collection = "productos")
 public class Producto {
     
+    @Id
     private Long idProducto; 
+    
     private String nombre;
     private String imgProducto;
     private TipoProducto tipoProducto;
